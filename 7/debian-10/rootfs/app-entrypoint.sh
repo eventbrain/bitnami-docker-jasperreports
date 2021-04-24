07d1478b6ace1b06f7616f65b0539f18273a760c
Sebastián Orellana <limcross@users.noreply.github.com>
2021-04-24 16:30:11 -0400

    feat: add support to copy custom config from jasperreports_data volume

--- 7/debian-10/rootfs/app-entrypoint.sh
+++ 7/debian-10/rootfs/app-entrypoint.sh
@@ -8,6 +8,11 @@ print_welcome_page
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     nami_initialize tomcat mysql-client jasperreports
     info "Starting gosu... "
+    if [ -d "/bitnami/custom-config/" ]; then
+        if ! [ -z "$(ls -A /bitnami/custom-config/)" ]; then 
+            cp -r "/bitnami/custom-config/" "/opt/bitnami/jasperreports/"
+        fi
+    fi
 fi
 
 exec tini -- "$@"
