99387441ce5b0313f4004cbee5e039bd7dcd922d
Sebastián Orellana <limcross@users.noreply.github.com>
2021-04-24 18:06:26 -0400

    fix: use the correct path where the volume is mounted

--- 7/debian-10/rootfs/app-entrypoint.sh
+++ 7/debian-10/rootfs/app-entrypoint.sh
@@ -8,9 +8,9 @@ print_welcome_page
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     nami_initialize tomcat mysql-client jasperreports
     info "Starting gosu... "
-    if [ -d "/bitnami/custom-config/" ]; then
-        if ! [ -z "$(ls -A /bitnami/custom-config/)" ]; then 
-            cp -r "/bitnami/custom-config/" "/opt/bitnami/jasperreports/"
+    if [ -d "/bitnami/jasperreports/custom-config/" ]; then
+        if ! [ -z "$(ls -A /bitnami/jasperreports/custom-config/)" ]; then 
+            cp -r "/bitnami/jasperreports/custom-config/" "/opt/bitnami/jasperreports/"
         fi
     fi
 fi
